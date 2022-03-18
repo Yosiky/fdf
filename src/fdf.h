228b6984b18794065b84983736889f570b4038a2
eestelle <yosiky@list.ru>
2022-03-18 17:52:36 +0300

    Begin write window path

--- src/fdf.h
+++ src/fdf.h
@@ -6,7 +6,7 @@
 /*   By: eestelle <eestelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 21:12:50 by eestelle          #+#    #+#             */
-/*   Updated: 2022/03/16 13:25:05 by eestelle         ###   ########.fr       */
+/*   Updated: 2022/03/18 17:35:19 by eestelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,11 @@
 # include "../libft/get_next_line.h"
 # include "../libft/libft.h"
 
-typedef struct s_data {
+# define WINDOW_WIDTH 1920
+# define WINDOW_HEIGHT 1080
+
+typedef struct s_data
+{
 	void		*img;
 	int			*addr;
 	int			bits_per_pixel;
@@ -28,6 +32,14 @@ typedef struct s_data {
 	int			endian;
 	t_matrix	*map;
 }	t_data;
+
+typedef struct s_position
+{
+	int	x;
+	int	y;
+	int	z;
+	int	angle[3];
+}	t_position;
 
 void		ee_exit(const char *str);
 t_matrix	*load_map(const char *name);
--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 /*   By: eestelle <eestelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 21:13:10 by eestelle          #+#    #+#             */
-/*   Updated: 2022/03/18 16:02:59 by eestelle         ###   ########.fr       */
+/*   Updated: 2022/03/18 17:35:04 by eestelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
 #include <stdio.h>
+
 void	print_matrix(t_matrix *map)
 {
 	printf("MAP:\n");
@@ -20,24 +21,43 @@ void	print_matrix(t_matrix *map)
 	printf("height = %d\n", map->height);
 	for (int i = 0; i < map->height; ++i)
 	{
-		for (int j = 0; j < map->width; ++j)
-		{
-			printf("\t%d", *(map->arr + i * map->height + j));
-		}
+        for (int j = 0; j < map->width; ++j)
+            printf("%3d", *(map->arr + i * map->width + j));
 		printf("\n");
 	}
 }
 
+static void	init_pos(t_position *a)
+{
+	a->x = 0;
+	a->y = 0;
+	a->z = 0;
+	a->angle[0] = 0;
+	a->angle[1] = 0;
+	a->angle[2] = 0;
+}
+
 int	main(int argc, char **argv)
 {
-	t_data	img;
+	t_data		img;
+	t_position	pos;
+	void		*mlx;
+	void		*mlx_win;
 
 	if (argc == 2)
 	{
 		img.map = load_map((const char *)argv[1]);
 		if (img.map == NULL)
 			ee_exit("Error: failed to allocate memory for the card\n");
-		//print_matrix(img.map);
+		print_matrix(img.map);
+		init_pos(&pos);
+		mlx = mlx_init();
+		mlx_win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "fdf");
+		mlx_loop(mlx);
+		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+		draw(&img, &pos);
+		mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+		mlx_loop(mlx);
 	    free_matrix(img.map);
     }
 	else
